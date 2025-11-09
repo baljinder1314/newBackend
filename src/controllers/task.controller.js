24d7ff32c6bc8c4080532b1cbc8cfea0d0f4c80c
Baljinder-Singh <baljindersingh987209@gmail.com>
2025-11-09 11:14:55 +0530

    Implement delete task functionality with error handling and route integration

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -123,3 +123,23 @@ export const updateTaskById = asyncHandler(async (req, res) => {
     });
   }
 });
+
+export const deleteTaskById = asyncHandler(async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      user_id: req.user?._id,
+    });
+    console.log("hahahahaha: ",task)
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json(new ApiResponse(200, {}, "Task deleted successfully"));
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting task", error: error.message });
+  }
+});
--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createTask,
+  deleteTaskById,
   getAllTasks,
   updateTaskById,
 } from "../controllers/task.controller.js";
@@ -11,3 +12,4 @@ export const taskRoutes = Router();
 taskRoutes.route("/create-task").post(authentication, createTask);
 taskRoutes.route("/get-all-tasks").get(authentication, getAllTasks);
 taskRoutes.route("/update-task-by-id/:id").put(authentication, updateTaskById);
+taskRoutes.route("/dalate-task-by-id/:id").delete(authentication,deleteTaskById)
